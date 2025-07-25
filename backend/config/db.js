9b044b4e74474a2edcc0b338cca5f4353dd3034b
zThienz <thanhthientv009@gmail.com>
2025-07-25 13:15:49 +0700

    tạo docker cho dự án

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'bcmh',
+});
+
+module.exports = pool;
